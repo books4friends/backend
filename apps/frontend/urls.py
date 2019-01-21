43eac783be7abaa0750675ea459ae6d6d9519ddb
Ruslan Sayfullin <uraltus@yandex.ru>
2019-01-21 21:56:16 +0500

    B-30 Vue.js Integration Refacoring

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -8,5 +8,8 @@ from .views import AppView
 urlpatterns = [
     url(r'^vk_redirect_uri/$', VkRedirectUrl.as_view(), name='vk-redirect-uri'),
     url(r'^$', TemplateView.as_view(template_name='frontend/login_form.html'), name='login-form'),
-    url(r'^app/*$', AppView.as_view(), name='app'),
+    url(r'^app/$', AppView.as_view(), name='app'),
+    url(r'^app/my-books/$', AppView.as_view(), name='app'),
+    url(r'^app/settings/$', AppView.as_view(), name='app'),
+    url(r'^app/about/$', AppView.as_view(), name='app'),
 ]
