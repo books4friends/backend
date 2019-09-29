83ea25edb818acd011214199aa0e6eefd2fa3417
Ruslan Sayfullin <uraltus@yandex.ru>
2019-09-29 19:26:18 +0500

    GBS-36 Test get settings

---
+++ apps/accounts/tests/test_get.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from apps.frontend.tests.utils import AuthMixin
+from apps.accounts.models import Account
+
+VK_ID = 'VK_ID'
+
+
+class AccountSettingsViewTest(TestCase, AuthMixin):
+    def setUp(self):
+        self.account = Account.objects.create(vk_id=VK_ID)
+
+    def test_url_exists_at_desired_location(self):
+        self.auth_user(self.account)
+        response = self.client.get('/app/api/settings/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_default_privacy(self):
+        self.auth_user(self.account)
+        response = self.client.get('/app/api/settings/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.account.privacy_type, Account.PRIVACY_TYPE.ALL_FRIENDS)
+
+    def test_only_owner_privacy(self):
+        self.auth_user(self.account)
+        self.account.privacy_type = Account.PRIVACY_TYPE.ONLY_OWNER
+        response = self.client.get('/app/api/settings/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.account.privacy_type, Account.PRIVACY_TYPE.ONLY_OWNER)
+
+
